c2b6453cdec660abd0d6980b4465f79a2ddbe6ce
Jessica Fix and Jordan Meier <jcfix.pdx@gmail.com>
2016-02-09 11:43:51 -0800

    add job opening form to app.php

--- app/app.php
+++ app/app.php
@@ -1,10 +1,47 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Job.php";
 
     $app = new Silex\Application();
 
     $app->get("/", function() {
-        return "Home";
+        return "
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css' integrity='sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7' crossorigin='anonymous'>
+                    <title>Post a Job Opening</title>
+                </head>
+                <body>
+                    <div class='container'>
+                        <h1>Post a Job Opening</h1>
+                        <form action='/view_jobs'>
+                            <div class='form-group'>
+                                <label for='title'>Enter Job Title:</label>
+                                <input id='title' name='title' class='form-control' type='text'>
+                            </div>
+                            <div class='form-group'>
+                                <label for='description'>Enter Job Description:</label>
+                                <input id='description' name='description' class='form-control' type='text'>
+                            </div>
+                            <div class='form-group'>
+                                <label for='salary'>Enter Job Salary:</label>
+                                <input id='salary' name='salary' class='form-control' type='text'>
+                            </div>
+                            <div class='form-group'>
+                                <label for='contact_name'>Enter Contact Name:</label>
+                                <input id='contact_name' name='contact_name' class='form-control' type='text'>
+                            </div>
+                            <div class='form-group'>
+                                <label for='contact_phone'>Enter Contact Phone #:</label>
+                                <input id='contact_phone' name='contact_phone' class='form-control' type='text'>
+                            </div>
+                            <button type='submit' class='btn-success'>Submit</button>
+                        </form>
+                    </div>
+                </body>
+            </html>
+            ";
     });
 
     return $app;
