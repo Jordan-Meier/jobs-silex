33725364c2bb1f7166a89db4706b8cb130a34048
Jessica Fix and Jordan Meier <jbirdmeier@gmail.com>
2016-02-09 13:21:17 -0800

    remove any contact info properties from Job class

--- src/Job.php
+++ src/Job.php
@@ -4,14 +4,14 @@ class Job
     private $title;
     private $description;
     private $salary;
-    private $contact;
 
-    function __construct($job_title, $job_description, $job_salary, $job_contact)
+
+    function __construct($job_title, $job_description, $job_salary)
     {
         $this->title = $job_title;
         $this->description = $job_description;
         $this->salary = $job_salary;
-        $this->contact = $contact;
+
     }
 
     function setTitle($new_title)
@@ -44,16 +44,16 @@ class Job
         return $this->salary;
     }
 
-
-    function setContact($new_contact)
-    {
-        $this->contact = $new_contact;
-    }
-
-    function getContact()
-    {
-        return $this->contact;
-    }
+    //
+    // function setContact($new_contact)
+    // {
+    //     $this->contact = $new_contact;
+    // }
+    //
+    // function getContact()
+    // {
+    //     return $this->contact;
+    // }
 
 }
 
