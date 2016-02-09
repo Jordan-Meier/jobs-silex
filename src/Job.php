96e2948057d0b2941cff84fc2ada1aa8d29a18f8
Jessica Fix and Jordan Meier <jcfix.pdx@gmail.com>
2016-02-09 11:31:37 -0800

    Add constructor to Job class

--- src/Job.php
+++ src/Job.php
@@ -7,9 +7,15 @@ class Job
     private $contact_name;
     private $contact_phone;
 
+    function __construct($job_title, $job_description, $job_salary, $job_contact_name, $job_contact_phone)
+    {
+        $this->title = $job_title;
+        $this->description = $job_description;
+        $this->salary = $job_salary;
+        $this->contact_name = $job_contact_name;
+        $this->contact_phone = $job_contact_phone;
 
-
-
+    }
 
 
 }
