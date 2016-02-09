ed132e5714a6cc34b74d81e69d7bb4a931de50b8
Jessica Fix and Jordan Meier <jcfix.pdx@gmail.com>
2016-02-09 13:29:17 -0800

    add contructor, getters and setters for Contact properties

--- src/Contact.php
+++ src/Contact.php
@@ -6,9 +6,42 @@
         private $number;
 
 
-        function __construct()
+        function __construct($contact_name, $contact_email, $contact_number)
+        {
+            $this->name = $contact_name;
+            $this->email = $contact_email;
+            $this->number = $contact_number;
+        }
 
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
 
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setEmail($new_email)
+        {
+            $this->email = $new_email;
+        }
+
+        function getEmail()
+        {
+            return $this->email;
+        }
+
+        function setNumber($new_number)
+        {
+            $this->number = $new_number;
+        }
+
+        function getNumber()
+        {
+            return $this->number;
+        }
 
     }
 
